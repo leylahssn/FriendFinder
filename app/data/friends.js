45cb1b9d84fefdc2b987236373c329619e588f01
Leyla Hussein <leylahssn2007@gmail.com>
2020-03-06 20:31:46 -0500

    adding questions

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+const friendsData = [
+    {
+        name: "Brittany",
+        score: 13
+    },
+    {
+        name: "Angela",
+        score: 15
+    },
+    {
+        name: "Mike",
+        score: 11
+    },
+    {
+        name: "Nile",
+        score: 20
+    }
+];
+
+module.exports = friendsData;
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+});
